7da7ffd12a8e5f4d6b4a4abfd8d09ff2b16f5a35
sathishrajaramadoss <43017578+sathishrajaramadoss@users.noreply.github.com>
2018-12-04 22:08:26 +1300

    Delete Total_Email_Reg.sql

--- Client Look-up/Src/Total_Email_Reg.sql
+++
@@ -1,9 +0,0 @@
-/*DECLARE @STARTDATE DATETIME,
-@ENDDATE DATETIME
-
-SET @STARTDATE ='06-01-2018'
-SET @ENDDATE =  '07-11-2018'*/
-
-SELECT count(id) as Total_Email_Users
-     FROM dbo.Consumer WHERE EmailAddress is not null and EmailAddress<>''
-	 and creationdate between convert(datetime, convert(varchar,@STARTDATE),112) and convert(datetime, convert(varchar,@ENDDATE),112)
